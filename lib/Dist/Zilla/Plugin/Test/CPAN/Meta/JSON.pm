e09383c8606dd7f61a86c3ac42eddffd4e976f20
Mike Doherty <doherty@cs.dal.ca>
2011-04-17 17:39:42 -0300

    Prune meta-json.t when META.json doesn't exist

--- lib/Dist/Zilla/Plugin/Test/CPAN/Meta/JSON.pm
+++ lib/Dist/Zilla/Plugin/Test/CPAN/Meta/JSON.pm
@@ -5,8 +5,9 @@ use warnings;
 # VERSION
 
 use Moose;
-use Moose;
+use Moose::Autobox;
 extends 'Dist::Zilla::Plugin::InlineFiles';
+with 'Dist::Zilla::Role::FilePruner';
 
 =head1 SYNOPSIS
 
@@ -17,7 +18,7 @@ In C<dist.ini>:
 =head1 DESCRIPTION
 
 This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
-following file:
+following file if F<META.json> is in your dist:
 
     xt/release/meta-json.t - a standard Test::CPAN::Meta::JSON test
 
@@ -25,10 +26,32 @@ See L<Test::CPAN::Meta::JSON> for what this test does.
 
 =cut
 
+sub prune_files {
+    my $self = shift;
+
+    # Bail if we find META.json
+    my $METAjson = 'META.json';
+    foreach my $file ($self->zilla->files->flatten) {
+        return if $file->name eq $METAjson;
+    }
+
+    # If META.json wasn't found, then prune out the test
+    my $test_filename = 'xt/release/meta-json.t';
+    foreach my $file ($self->zilla->files->flatten) {
+        next unless $file->name eq $test_filename;
+
+        $self->zilla->prune_file($file);
+        $self->log_debug([ '%s not found; pruning %s', $METAjson, $file->name ]);
+    }
+    return;
+}
+
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+=for Pod::Coverage prune_files
 
 =for test_synopsis
 1;
